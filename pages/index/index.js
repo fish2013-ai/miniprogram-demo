b742de51f19c823a7aa5fa7468be11c323910ed5
tanchaoju <1120153473@qq.com>
2020-01-16 18:05:31 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,8 @@ Page({
    */
   data: {
     imgData: [], //轮播图数据
+    catitems: [], //导航菜单数据
+    floordata: {}, //楼层数据
     indicatorDots: true, //是否显示面板指示点
     vertical: false, //滑动方向是否为纵向
     autoplay: true, //是否自动切换
@@ -14,63 +16,83 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    //轮播图数据
     wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata', //仅为示例，并非真实的接口地址
-      success:res=> {
-        this.setData({
-          imgData:res.data.message
-        })
-      }
-    })
+        url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata', //轮播图数据的接口地址
+        success: res => {
+          this.setData({
+            imgData: res.data.message
+          })
+        }
+      }),
+      //导航菜单数据
+      wx.request({
+        url: 'https://api.zbztb.cn/api/public/v1/home/catitems', //导航菜单数据的接口地址
+        success: res => {
+          this.setData({
+            catitems: res.data.message
+          })
+        }
+      }),
+      // 楼层数据
+      wx.request({
+        url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+        success: res => {
+          this.setData({
+            floordata:res.data.message
+          })
+          console.log(this.data.floordata)
+        }
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
