c28b3f23289a85db55d9405528675cca9c761650
tanchaoju <1120153473@qq.com>
2020-01-17 15:32:12 +0800

    商品分类页面数据动态渲染完成

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,14 +6,12 @@ Page({
    */
   data: {
     categoriesData: [], // 左边列表数据
-    goodsData: [] // 右边商品数据
-
+    goodsData: [], // 右边商品数据
+    currentIndex: 0,
+    goodsIndex: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
+  // 将获取数据封装为函数
+  getData() {
     // 请求分类数据
     wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/categories',
@@ -21,62 +19,77 @@ Page({
         // 构造左边列表数据
         let categoriesData = res.data.message.map(v => v.cat_name)
         //  右边商品数据
-        let goodsData = res.data.message[0].children
+        let goodsData = res.data.message[this.data.goodsIndex].children
         this.setData({
           categoriesData,
           goodsData
         })
-        console.log(this.data.goodsData)
       }
     })
+  },
+  // 点击左边菜单时触发 
+  handleMenuTap(e) {
+    const index = e.currentTarget.dataset.index
+    this.setData({
+      currentIndex: index,
+      goodsIndex: index
+    })
+    this.getData()
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getData()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
